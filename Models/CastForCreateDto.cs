686090702e3562f810fd9f8799918b470de98a27
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2021-09-08 20:48:49 -0300

    create Dto of creation

---
+++ Models/CastForCreateDto.cs
@@ -0,0 +1,9 @@
+namespace api.Models
+{
+    public class CastForCreateDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Character { get; set; }
+    }
+}
