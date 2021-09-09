a948f7d5e8fbe0a1220c5f4a69e4e2a9a45709b4
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2021-09-08 21:10:24 -0300

    add DataAnotations

--- Models/CastDto.cs
+++ Models/CastDto.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace api.Models
 {
     public class CastDto
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Character { get; set; }
 
     }
