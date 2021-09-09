ef99c5db9a0f6a913ad6e623f71e0d89ae9552b0
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2021-09-08 21:24:02 -0300

    created Dto the update

--- Models/CastForUpdateDto.cs
+++ Models/CastForUpdateDto.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace api.Models
 {
-    public class CastForCreateDto
+    public class CastForUpdateDto
     {
         public int Id { get; set; }
 
