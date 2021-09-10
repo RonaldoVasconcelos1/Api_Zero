e3905b75b7f28c02f8ab00611bcb76f18d0e13be
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2021-09-10 10:57:37 -0300

    migrations of seeds

---
+++ Migrations/20210910135329_SampleData.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class SampleData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 1, "Isosklasjsjladk", "Pandas de dedede" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 2, "2331", "Kung fu" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 3, "Kid", "Karate" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 4, "Nos Bota Nela", "Eu tu" });
+
+            migrationBuilder.InsertData(
+                table: "Casts",
+                columns: new[] { "Id", "Age", "Character", "MovieId", "Name" },
+                values: new object[] { 1, null, "Ele memo", 1, "Daniel luis" });
+
+            migrationBuilder.InsertData(
+                table: "Casts",
+                columns: new[] { "Id", "Age", "Character", "MovieId", "Name" },
+                values: new object[] { 3, null, "Isso", 1, "Couto amem" });
+
+            migrationBuilder.InsertData(
+                table: "Casts",
+                columns: new[] { "Id", "Age", "Character", "MovieId", "Name" },
+                values: new object[] { 2, null, "Ele memo", 2, "Jose luis" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Casts",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Casts",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Casts",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
