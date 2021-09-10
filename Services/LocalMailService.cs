9199bdb5f3ff113d79cd333dee439fbc90656bbe
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2021-09-09 21:06:22 -0300

    refactor

--- Services/LocalMailService.cs
+++ Services/LocalMailService.cs
@@ -1,15 +1,21 @@
 using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+
 namespace api.Services
 {
-    public class LocalMailService
-    {
-        private string _mailTo = "admin@company.com";
 
-        private string _mailFrom = "noreply@company.com";
+    public class LocalMailService : IMailService
+    {
+        private IConfiguration _configuration;
+
+        public LocalMailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         public void Send(string subject, string message)
         {
-            Debug.Write($"Email send {_mailFrom} and {_mailTo}");
+            Debug.Write($"Email send {_configuration["mailSettings:mailToAdsress"]} and {_configuration["mailSettings:mailFromAddress"]} LocalMailService");
             Debug.Write($"Subject: {subject}");
             Debug.Write($"Message: {message}");
         }
--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,8 @@ namespace api
                 }
             ).AddXmlDataContractSerializerFormatters();
 
-            services.AddTransient<LocalMailService>();
+            services.AddTransient<IMailService, LocalMailService>();
+            services.AddTransient<IMailService, CloudLocalService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
